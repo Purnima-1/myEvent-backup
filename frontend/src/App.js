e39c4f7f022d18a49ab4abc15c81ddaeb8db55e1
Purnima-1 <baranwal.purnimagmail.com>
2022-03-10 14:23:05 -0800

    Another eventscreen component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import { Container } from "react-bootstrap";
 import { BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import EventScreen from "./screens/EventScreen";
+// import EventScreen from "./screens/EventScreen";
+import EventScreen1 from "./screens/EventScreen1";
 //import HomePageImg from "./components/HomePageImg";
 import HomeScreen from "./screens/HomeScreen";
 
@@ -17,7 +18,7 @@ function App() {
    <Container>
      <Routes>
        < Route path='/' element={<HomeScreen />} exact />
-       <Route path= '/event/:id' element={<EventScreen />} />
+       <Route path= '/event/:id' element={<EventScreen1 />} />
      </Routes>
      </Container>
     </main>
---
+++ frontend/src/screens/EventScreen1.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useParams, Link } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  Dropdown,
+} from "react-bootstrap";
+import events from "../events";
+
+const EventScreen1 = () => {
+  const params = useParams();
+  const event = events.find((e) => e._id === params.id);
+  return (
+    <>
+      <div className="container">
+        <Row class="justify-content-md-center">
+          <Col class="column" md={9} xs={12} sm={12}>
+            <Image src={event.image1} alt={event.name} />
+          </Col>
+          <Col class="column" md={3} xs={12} sm={12}>
+            <ListGroup>
+              <ListGroup.Item>{event.schedule.slice(5, 11)}</ListGroup.Item>
+              <ListGroup.Item>
+                <h3>{event.title}</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <p style={{ color: "red" }}>by {event.name}</p>
+                <p>{event.followers} followers</p>
+              </ListGroup.Item>
+
+              <ListGroup.Item>Price: ${event.adtprice?event.adtprice: "Free"}</ListGroup.Item>
+            <ListGroup.Item>Price: ${event.kidprice?event.kidprice: "Free"}</ListGroup.Item>    
+             
+              </ListGroup>
+            <Button
+              className="btn-block"
+              type="button"
+              disabled={event.tickets === 0}
+            >
+             {event.price === "Free" ? "Register" : "Tickets"}
+            </Button>
+          </Col>
+        </Row>
+        <hr />
+
+        <Row>
+          <Col md={9} xs={12} sm={12}>
+        <ListGroup.Item>
+          <h5>{event.description}</h5>{" "}
+        </ListGroup.Item>
+        </Col>
+        <Col md={3} xs={12} sm={12}>
+        <p>Date and time</p>
+        <p>{event.schedule}</p>
+        <p>Location <br/> {event.Address}</p>
+        <Link to ={`/event/${event._id}`}>
+      <iframe src= {event.source} variant='top'/>
+        </Link>
+        {/* <iframe src={event.source} >View Map</iframe> */}
+        </Col>
+        </Row>
+      </div>
+    </>
+  );
+};
+
+export default EventScreen1;
