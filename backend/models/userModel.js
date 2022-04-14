d99bc042ed19b535076cbe5c6311fcb258ca4ea7
Purnima-1 <baranwal.purnimagmail.com>
2022-04-14 12:04:26 -0700

    commented out localstorage, was giving json parse error

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -21,6 +21,13 @@ isAdmin:{
     default: false
 }
 },{ timestamps: true })
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+      next()
+    }
+    const salt = await bcryptjs.genSalt(10)
+    this.password = await bcryptjs.hash(this.password, salt)
+  })
 userSchema.methods.matchPassword = async function(enteredPassword){
     return await bcryptjs.compare(enteredPassword,this.password)
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -20,17 +20,17 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer
 });
 
-const cartItemsFromStorage = localStorage.getItem("cartItems")
-  ? JSON.parse(localStorage.getItem("cartItems"))
-  : [];
+// const cartItemsFromStorage = localStorage.getItem("cartItems")
+//   ? JSON.parse(localStorage.getItem("cartItems"))
+//   : [];
 
-  const userInfoFromStorage = localStorage.getItem("userInfo")
-  ? JSON.parse(localStorage.getItem("userInfo"))
-  : null;
+//   const userInfoFromStorage = localStorage.getItem("userInfo")
+//   ? JSON.parse(localStorage.getItem("userInfo"))
+//   : null;
 
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
-  userLogin: {userInfo: userInfoFromStorage}
+  // cart: { cartItems: cartItemsFromStorage },
+  // userLogin: {userInfo: userInfoFromStorage}
 };
 const middleware = [thunk];
 const store = createStore(
